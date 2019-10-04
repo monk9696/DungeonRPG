04399c39688b0e4d7f4d0c95f6fb3cd4d7fccf0c
monk9696 <nkrems@gmail.com>
2019-10-04 14:30:43 -0400

    Wizard Start

---
+++ classes/WizardClass.java
@@ -0,0 +1,69 @@
+package classes;
+
+import characters.Character;
+
+public class WizardClass extends GenClass {
+
+	public String getSpellName(int spell) {
+		switch (spell){
+			case 1:
+				return "Magic Missile";
+			case 2:
+				return "Fireball";
+			case 3:
+				return "Reflect";
+			case 4:
+				return "Word of Healing ";
+			case 5: 
+				//return "Whirlwind ";
+			default:
+				return "Not a valid spell";
+		}
+	}
+
+	protected boolean spell1(Character user, Character target) {
+		if(spell.checkCooldown(0)) {
+			spell.setCooldown(0, 3);
+			spell.mMissile(user, target);
+			return true;
+		}
+		return false;
+	}
+
+	protected boolean spell2(Character user, Character target) {
+		if(spell.checkCooldown(1)) {
+			spell.setCooldown(1, 3);
+			spell.fireball(user, target);
+			return true;
+		}
+		return false;
+	}
+
+	protected boolean spell3(Character user, Character target) {
+		if(spell.checkCooldown(2)) {
+			spell.setCooldown(2, 5);
+			spell.reflect(user, target);
+			return true;
+		}
+		return false;
+	}
+
+	protected boolean spell4(Character user, Character target) {
+		if(spell.checkCooldown(3)) {
+			spell.setCooldown(3, 5);
+			spell.wordOfHealing(user, target);
+			return true;
+		}
+		return false;
+	}
+
+	protected boolean spell5(Character user, Character target) {
+		if(spell.checkCooldown(4)) {
+			spell.setCooldown(4, 5);
+			spell.reflect(user, target);
+			return true;
+		}
+		return false;
+	}
+
+}
